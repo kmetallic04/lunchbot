4733f74c6709108ecc26a6966db08e753c522734
Jodom <jodomouma@gmail.com>
2018-08-27 16:57:28 +0300

    intergrate mongoose

---
+++ models/db.setup.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+require('dotenv').load();
+
+
+const options = {
+    useNewUrlParser: true,
+    reconnectTries: 50,
+    reconnectInterval: 500,
+    poolSize: 10,
+    bufferMaxEntries: 0
+}
+
+function connectDB(database) {
+    mongoose.connect(process.env.DATABASE_URI + database, options)
+        .then(function () {
+            console.log(database + ' connection successful');
+        })
+        .catch(function (error) {
+            console.error.bind(console, database + ' connection unsuccessful, retrying in 5 seconds : ')
+        });
+    mongoose.Promise = global.Promise;
+
+    return mongoose.connection;
+}
+
+module.exports = connectDB;
