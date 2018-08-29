d15d95b4275c7f0f586c647ea37755f12ded8f82
Jodom <jodomouma@gmail.com>
2018-08-29 20:35:52 +0300

    vendor api routes

---
+++ routes/vendors.js
@@ -0,0 +1,127 @@
+const express  = require('express');
+const router   = express.Router();
+const validate = require('validate.js')
+
+const {
+    getAll,
+    get,
+    create,
+    update,
+    delete_
+} = require('../models/utils');
+
+const {
+    log,
+    sendServerError,
+    sendValidationError,
+    sendResults
+} = require('../src/utils');
+
+const _validateParams = (params) => {
+    let validationError;
+
+    const constraints = {
+        name: function (value) {
+            if (value && !validate.isString(value)) {
+                return {
+                    format: {
+                        message: 'must be a string'
+                    }
+                }
+            }
+            return {
+                presence: {
+                    message: 'is required'
+                }
+            }
+        }
+    }
+
+    const error = validate(params, constraints);
+
+    const makeErrorMessage = function (error) {
+        let msg = "";
+        for (let k in error) {
+            msg += error[k] + "; ";
+        }
+        validationError = new Error(msg);
+    }
+    if (error) {
+        makeErrorMessage(error)
+    }
+    return validationError;
+}
+
+router.get('/', (req, res) => {
+    getAll('vendor')
+        .then( result => {
+            sendResults(res, result);
+        })
+        .catch(err => {
+            log.error(err);
+            sendServerError(res, err);
+        });
+});
+
+router.get('/vendor/:name', (req, res) => {
+    const name = req.params.name;
+    get('vendor', name)
+        .then(result => {
+            sendResults(res, result);
+        })
+        .catch(err => {
+            log.error(err);
+            sendServerError(res, err);
+        });
+});
+
+router.post('/create', (req, res) => {
+    const details = req.body;
+    const validationError = _validateParams(details);
+
+    if (validationError) {
+        sendValidationError(res, validationError);
+    }
+
+    create('vendor', details)
+        .then(result => {
+            sendResults(res, result);
+        })
+        .catch(err => {
+            log.error(err);
+            sendServerError(res, err);
+        });
+});
+
+router.put('/edit/:id', (req, res) => {
+    const updates = req.body;
+    const id = req.params.id;
+    const validationError = _validateParams(updates);
+
+    if (validationError) {
+        sendValidationError(res, validationError);
+    }
+
+    update('vendor', id, updates)
+        .then(result => {
+            sendResults(res, result);
+        })
+        .catch(err => {
+            log.error(err);
+            sendServerError(res, err);
+        });
+});
+
+router.delete('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    delete_('vendor', id)
+        .then(result => {
+            sendResults(res, result);
+        })
+        .catch(err => {
+            log.error(err);
+            sendServerError(res, err);
+        });
+});
+
+module.exports = router;
